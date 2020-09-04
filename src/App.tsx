c4bd10d945455b6a07f0357438f24811bab86bd1
Marcos NC <marcosnc@gmail.com>
2020-09-04 19:49:51 -0300

    Exploring options for the game loop.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+/* Option 1: Using simple game loop and setInterval
+var callNumber:number = 0;
+function gameLoop() {
+    callNumber++;
+    const now = new Date()
+    console.log(callNumber, now)
+}
+setInterval(gameLoop, 1);
+*/
+
+// /* Option 2: Using GameLoop component and window.requestAnimationFrame
+class GameLoop extends React.Component {
+    private animationID: number|null;
+    private callNumber: number;
+    constructor(props: any) {
+        super(props);
+        this.animationID = null;
+        this.callNumber = 0;
+    }
+
+    componentDidMount() {
+        this.animationID = window.requestAnimationFrame(() => this.update());
+    }
+    componentWillUnmount() {
+        if (this.animationID) {
+            window.cancelAnimationFrame(this.animationID);
+        }
+    }
+
+    update() {
+        this.callNumber++;
+        const now = new Date()
+        console.log(this.callNumber, now)
+        this.animationID = window.requestAnimationFrame(() => this.update());
+    }
+
+    render() {
+        return(
+            <div>The Game Loop</div>
+        )
+    }
+}
+// */
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <canvas id="myCanvas" width="500" height="500"></canvas>
+            <GameLoop />
+        </div>
+    );
 }
 
 export default App;
